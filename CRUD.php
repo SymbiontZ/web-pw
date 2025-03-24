b1f10c36e7d39a83de9eced504f41617f1d28a8b
Julia Muñoz <127932207+jjulsmt@users.noreply.github.com>
2025-03-24 17:02:57 +0100

    Update CRUD.php
    Todas las operaciones funcionan, lo próximo es el tema de sacar las fotos de cada libro para ponerlas en la página

--- CRUD.php
+++ CRUD.php
@@ -4,11 +4,25 @@
         $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Conexion exitosa<br><br>";
 
-        $resultado = $base->query('SELECT titulo, autor, paginas, fecha_pub FROM libros');
-        while($datos = $resultado->fetch())
+        //Lectura de datos
+        $lectura = $base->query('SELECT titulo, autor, paginas, fecha_pub FROM libros');
+        while($datos = $lectura->fetch())
         {
             echo "Titulo: " . $datos['titulo'] . " Autor: " . $datos['autor'] . " Paginas: " . $datos['paginas'] . " Fecha de publicacion: " . $datos['fecha_pub'] . "<br><br>";
         }
+
+        //Inserción de datos
+        //$insercion = $base->exec("INSERT INTO libros (categoria, titulo, autor, paginas, fecha_pub) VALUES ('2','El señor de los anillos', 'J.R.R. Tolkien', '1200', '1954-07-29')");
+        //echo "Insercion exitosa<br><br>";
+
+        //Eliminación de datos
+        //$eliminacion = $base->exec("DELETE FROM libros WHERE titulo = 'La asistenta'");
+        //echo "Eliminacion exitosa<br><br>";
+
+        //Actualización de datos
+        //$actualizacion = $base->exec("UPDATE libros SET paginas = '1200' WHERE titulo = 'El señor de los anillos'");
+        //echo "Actualizacion exitosa<br><br>";
+
     }catch(Exception $e){
         die('Error: ' . $e->GetMessage());
     }finally{
