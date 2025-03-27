0feeaa6e6be999bb14a4e10ad2e23a547e87aeaa
FabioR <fabiorrojas56@gmail.com>
2025-03-27 19:32:36 +0100

    fix clases

--- src/clases.php
+++ src/clases.php
@@ -3,21 +3,21 @@
 class Libro{
     private string $titulo;
     private string $autor;
-    private int $numPags;
+    private int $paginas;
     private string $fecha;
-    private string $url;
+    private string $imagen;
     private float $precio;
     private array $categorias;
 
 
 
-    public function __construct(string $titulo, string $autor,float $precio, int $numPags, string $fecha, string $url, array $categorias) {
+    public function __construct(string $titulo = '', string $autor = '',float $precio = 0.0, int $paginas = 0, string $fecha = '', string $imagen = '', array $categorias = []) {
         $this->titulo = $titulo;
         $this->autor = $autor;
         $this->precio = $precio;
-        $this->numPags = $numPags;
+        $this->paginas = $paginas;
         $this->fecha = $fecha;
-        $this->url = $url;
+        $this->imagen = $imagen;
         $this->categorias = $categorias;
     }
 
@@ -25,18 +25,18 @@ class Libro{
     public function get_titulo(): string { return $this->titulo; }
     public function get_autor(): string { return $this->autor; }
     public function get_precio(): float { return $this->precio; }
-    public function get_numPags(): int { return $this->numPags; }
+    public function get_numPags(): int { return $this->paginas; }
     public function get_fecha(): string { return $this->fecha; }
-    public function get_url(): string { return $this->url; }
+    public function get_url(): string { return $this->imagen; }
     public function get_categorias(): array { return $this->categorias; }
 
     // Setters
     public function set_titulo(string $titulo): void { $this->titulo = $titulo; }
     public function set_autor(string $autor): void { $this->autor = $autor; }
     public function set_precio(float $precio): void { $this->precio = $precio; }
-    public function set_numPags(int $numPags): void { $this->numPags = $numPags; }
+    public function set_numPags(int $numPags): void { $this->paginas = $numPags; }
     public function set_fecha(string $fecha): void { $this->fecha = $fecha; }
-    public function set_url(string $url): void { $this->url = $url; }
+    public function set_url(string $imagen): void { $this->imagen = $imagen; }
     public function set_categorias(array $categorias): void { $this->categorias = $categorias; }
 
 }
