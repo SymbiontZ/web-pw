48758379272f318ddca58cddab4268be7e063f1e
FabioR <fabiorrojas56@gmail.com>
2025-04-01 10:37:10 +0200

    update index + css

--- index.php
+++ index.php
@@ -9,6 +9,15 @@
 
         switch ($orden) {
             case 'descarga':
+                $lista[] = new Libro(
+                    "El Señor de los Anillos", 
+                    "J.R.R. Tolkien",
+                    19.99,
+                    1216, 
+                    "1954-07-29", 
+                    "el-senor-de-los-anillos.jpg", 
+                    ["Fantasía", "Aventura"]
+                );
                 break;
             case 'fecha':
                 $lista[] = new Libro(
@@ -27,16 +36,26 @@
         }
         
         if(count($lista) > 0){
+            echo "<div class='products-container'>";
             foreach ($lista as $libro) {
-                echo "<div class='product-container'>";
-                echo "<img src='./data/". $libro->get_url()."' width='200' style='margin:5px;'><br>";
-                echo "<hr>";
-                echo "<h3>" . htmlspecialchars($libro->get_titulo()) . "</h3>";
-                echo "<p>Autor: " . htmlspecialchars($libro->get_autor()) . "</p>";
-                echo "<p>Precio: $" . number_format($libro->get_precio(), 2) . "</p>";
+                echo "<div class='product-list d-flex justify-center'>";
+                echo "  <div class='product-container justify-center max-w'";
+                echo "      <a  href='https://example.com'>";
+                echo "          <img class = 'justify-center d-flex max-w' src='./data/". $libro->get_url()."' alt='".$libro->get_titulo()."-".$libro->get_autor()."'>";
+                echo "      </a>";
+                echo "      <hr>";
+                
+                echo "      <div>";
+                echo "          <a class='size-28 bold no-link-style' href='https://example.com'>" . htmlspecialchars(strtoupper($libro->get_titulo())) . "</a>";
+                echo "          <p class='size-20 low-margin-v'>" . htmlspecialchars($libro->get_autor()) . "</p>";
+                echo "          <p>" . number_format($libro->get_precio(), 2) . "€ </p>";
+                echo "      </div>";
+                echo "  </div>";
 
+                
                 echo "</div>";
             }
+            echo "</div>";
         }else{
             echo "No hay resultados de esta busqueda :(";
         }
@@ -55,18 +74,19 @@
 </head>
 <body>
     <div class="navbar">
-        <button class="nav-btn raleway-regular">WANNABOOK</button>
+        <button href="./" class="nav-btn raleway-regular">WANNABOOK</button>
     </div>
-    <br/>
-    <br/>
 
-    <div id="section">
-        Mas vendidos
-        <a href="./">Ver más</a>
+    <div class="section first-section">
+        <div class="align-center d-flex">
+            <p class="vertical-text" >Más vendidos</p>
+            <a class="vertical-text" href="./">Ver más</a>
+        </div>
         <hr>
+        <?php mostrar_libros('descarga');?>
     </div>
     <div>
-        <?php mostrar_libros('descarga');?>
+        
         <h2>Libros mas recientes</h2>
         <?php mostrar_libros('fecha');?>
     </div>
