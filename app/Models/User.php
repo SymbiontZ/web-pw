5745fe75758bbd6b5256afe9b0eeb0ea8ba5f38a
Mariopeve <mariopeve5971@gmail.com>
2025-05-10 15:42:59 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    public $timestamps = false;
+
     protected $table = 'usuarios';
     /**
      * The attributes that are mass assignable.
