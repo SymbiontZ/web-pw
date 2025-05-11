1aac3f4db3076a5a7426883a9b1bd8012e5f0bc8
jjulsmt <127932207+jjulsmt@users.noreply.github.com>
2025-05-11 17:54:40 +0200

    Reviews finalizadas He añadido que el usuario no pueda hacer la review si no está logueado y he cambiado el tema de la puntuacion para que sea un desplegable del 1 al 5

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Review;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller;
 
 class ReviewController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index']);
+    }
+
     public function guardar(Request $request)
     {
+
         $validated = $request->validate([
             'libro_id' => 'required|exists:libros,id_libro', 
             'usuario' => 'required|string|max:255',
@@ -18,7 +26,7 @@ class ReviewController extends Controller
 
         Review::create([
             'libro_id' => $validated['libro_id'],
-            'usuario' => $validated['usuario'],
+            'usuario' => Auth::user()->nombre,
             'review' => $validated['review'], 
             'puntuacion' => $validated['puntuacion'], 
         ]);
